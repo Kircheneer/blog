49548fdbe7c0c868736a98e44fb011b5f54c88bc
Leo Kirchner <leo@kirchne.red>
2021-04-15 21:32:00 +0200

    Update

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -4,32 +4,29 @@
 AUTHOR = 'Leo Kirchner'
 SITENAME = 'Kirchnered Networking'
 SITEURL = ''
-
 PATH = 'content'
-
+STATIC_PATHS = ['images']
 TIMEZONE = 'Europe/Berlin'
-
-DEFAULT_LANG = 'English'
-
-# Feed generation is usually not desired when developing
+DEFAULT_LANG = 'en'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
 THEME = "Flex"
-
-# Blogroll
 LINKS = (('Home', ''),)
-
-# Social widget
 SOCIAL = (
     ("github", "https://github.com/Kircheneer"),
     ("rss", "/blog/feeds/all.atom.xml"),
 )
-
 DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+FAVICON = '/images/favicon.ico'
+SITELOGO = SITEURL + "/images/profile.png"
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+BROWSER_COLOR = "#777"
